0d3d84e7f7cc4c31faa822bbc2c4b3b0de926e65
SteffenP <pfahnl.steffen@gmail.com>
2023-10-31 15:15:07 -0500

    Updated backend

--- Root Module/terraform.tf
+++ Root Module/terraform.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
     backend "s3" {
-        bucket         = "terraform-remote-backend-57"
+        bucket         = "terraform-s3-remote-state-steffen"
         key            = "global/backend/terraform.tfstate"
         region         = "us-east-1"
         dynamodb_table = "terraform-state-locking"
